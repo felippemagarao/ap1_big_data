4c43ba442c3095666875e0adbadc2b62160b2fe4
Felippe Magarao <felippemagarao@gmail.com>
2024-09-30 19:17:19 -0300

    inclusao dos testes de unidade

--- src/test/java/br/edu/ibmec/felippe/trabalho_ap1/controller/ClienteControllerTest.java
+++ src/test/java/br/edu/ibmec/felippe/trabalho_ap1/controller/ClienteControllerTest.java
@@ -1,5 +1,137 @@
 package br.edu.ibmec.felippe.trabalho_ap1.controller;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.edu.ibmec.felippe.trabalho_ap1.model.Cliente;
+import br.edu.ibmec.felippe.trabalho_ap1.repository.ClienteRepository;
+import br.edu.ibmec.felippe.trabalho_ap1.service.ClienteService;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.hamcrest.Matchers.is;
+
+@AutoConfigureMockMvc
+@WebMvcTest(controllers = ClienteController.class)
 public class ClienteControllerTest {
-  
+
+  @MockBean
+  private ClienteRepository clienteRepository;
+
+  @MockBean
+  private ClienteService clienteService;
+
+  @Autowired
+  private WebApplicationContext context;
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  @BeforeEach
+  public void setup() {
+    this.mvc = MockMvcBuilders
+      .webAppContextSetup(context)
+      .build();
+  }
+
+  @Test
+  public void should_create_cliente() throws Exception {
+    Cliente cliente = new Cliente();
+    cliente.setId(0);
+    cliente.setNome("Teste");
+    cliente.setCpf("999.000.111-22");
+    cliente.setDataNascimento(LocalDate.of(2000, 01, 02));
+    cliente.setEmail("teste@teste.com");
+    cliente.setTelefone("(00) 9999-7777");
+    cliente.setEnderecos(null);
+    
+    given(this.clienteService.adicionarCliente(cliente)).willReturn(cliente);
+
+    this.mvc.perform(MockMvcRequestBuilders
+        .post("/clientes")
+        .content(this.mapper.writeValueAsString(cliente))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())  // Status 200, pois ResponseEntity.ok() é usado
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(0)))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.nome", is("Teste")));
+  }
+
+  @Test
+  public void should_get_cliente() throws Exception {
+    Cliente cliente = new Cliente();
+    cliente.setId(1);
+    cliente.setNome("Lorem Ipsum One");
+    cliente.setCpf("000.777.888-44");
+    cliente.setDataNascimento(LocalDate.of(2000, 01, 03));
+    cliente.setEmail("lorem@ipsom.com");
+    cliente.setTelefone("(99) 8888-8787");
+    cliente.setEnderecos(null);
+
+    given(this.clienteRepository.findById(1)).willReturn(Optional.of(cliente));
+
+    this.mvc.perform(MockMvcRequestBuilders
+        .get("/clientes/1")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(1))); 
+  }
+
+  @Test
+  public void should_get_cliente_with_not_found() throws Exception {
+    // Mock para cliente não encontrado
+    given(this.clienteRepository.findById(1)).willReturn(Optional.empty());
+
+    this.mvc.perform(MockMvcRequestBuilders
+        .get("/clientes/1")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isNotFound());
+  }
+
+  @Test
+  public void should_delete_cliente() throws Exception {
+    // Mock para remover cliente
+    doNothing().when(clienteService).removerCliente(1);
+
+    this.mvc.perform(MockMvcRequestBuilders
+        .delete("/clientes/1")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isNoContent());  // Status 204 esperado para DELETE
+  }
+
+  @Test
+  public void should_update_cliente() throws Exception {
+    Cliente cliente = new Cliente();
+    cliente.setId(1);
+    cliente.setNome("Nome Atualizado");
+    cliente.setCpf("111.111.111-11");
+    cliente.setEmail("atualizado@teste.com");
+
+    given(clienteService.atualizarCliente(1, cliente)).willReturn(cliente);
+
+    this.mvc.perform(MockMvcRequestBuilders
+        .put("/clientes/1")
+        .content(mapper.writeValueAsString(cliente))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.nome", is("Nome Atualizado")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.cpf", is("111.111.111-11")));
+  }
 }
--- src/test/java/br/edu/ibmec/felippe/trabalho_ap1/controller/EnderecoControllerTest.java
+++ src/test/java/br/edu/ibmec/felippe/trabalho_ap1/controller/EnderecoControllerTest.java
@@ -1,5 +1,105 @@
 package br.edu.ibmec.felippe.trabalho_ap1.controller;
 
+import br.edu.ibmec.felippe.trabalho_ap1.model.Cliente;
+import br.edu.ibmec.felippe.trabalho_ap1.model.Endereco;
+import br.edu.ibmec.felippe.trabalho_ap1.service.ClienteService;
+import br.edu.ibmec.felippe.trabalho_ap1.service.EnderecoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+
+@WebMvcTest(EnderecoController.class)
+@AutoConfigureMockMvc
 public class EnderecoControllerTest {
-  
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private EnderecoService enderecoService;
+
+    @MockBean
+    private ClienteService clienteService;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        this.mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void should_add_endereco() throws Exception {
+        Endereco endereco = new Endereco();
+        endereco.setId(1);
+        endereco.setRua("Rua Teste");
+        endereco.setCidade("Cidade Teste");
+        endereco.setCep("00000-000");
+
+        Cliente cliente = new Cliente();
+        cliente.setId(1);
+        cliente.setNome("Cliente Teste");
+
+        given(clienteService.buscarCliente(1)).willReturn(Optional.of(cliente));
+        given(enderecoService.adicionarEndereco(endereco, cliente)).willReturn(endereco);
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/clientes/1/enderecos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(endereco)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.rua").value("Rua Teste"));
+    }
+
+    @Test
+    public void should_update_endereco() throws Exception {
+        Endereco endereco = new Endereco();
+        endereco.setId(1);
+        endereco.setRua("Rua Atualizada");
+        endereco.setCidade("Cidade Atualizada");
+        endereco.setCep("00000-111");
+
+        given(enderecoService.atualizarEndereco(1, endereco)).willReturn(endereco);
+
+        mvc.perform(MockMvcRequestBuilders
+                .put("/clientes/1/enderecos/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(endereco)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.rua").value("Rua Atualizada"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.cep").value("00000-111"));
+    }
+
+    @Test
+    public void should_delete_endereco() throws Exception {
+        doNothing().when(enderecoService).removerEndereco(1);
+
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/clientes/1/enderecos/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
 }
+
