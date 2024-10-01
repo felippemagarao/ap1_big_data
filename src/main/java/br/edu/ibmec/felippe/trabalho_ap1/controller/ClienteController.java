2b0bc60b9cc2023cc6751c811770b0b4ca84e660
Felippe Magarao <felippemagarao@gmail.com>
2024-10-01 13:05:59 -0300

    conexão banco de dados e correção actions

--- src/main/java/br/edu/ibmec/felippe/trabalho_ap1/controller/ClienteController.java
+++ src/main/java/br/edu/ibmec/felippe/trabalho_ap1/controller/ClienteController.java
@@ -1,7 +1,8 @@
 package br.edu.ibmec.felippe.trabalho_ap1.controller;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ibmec.felippe.trabalho_ap1.model.Cliente;
@@ -29,10 +31,14 @@ public class ClienteController {
   }  
 
 
-  @GetMapping
-  public List<Cliente> listarClientes(){
-    return clienteRepository.findAll();
-  }
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Cliente>> buscarCliente(@PathVariable int id) {
+    Optional<Cliente> cliente = clienteService.buscarCliente(id);
+    if (cliente == null) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    return ResponseEntity.ok(cliente);
+}
 
   @PostMapping
   public ResponseEntity<Cliente> adicionarCliente(@RequestBody Cliente cliente) throws Exception {
@@ -48,6 +54,7 @@ public class ClienteController {
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void removerCliente (@PathVariable int id){
     clienteService.removerCliente(id);
   }
